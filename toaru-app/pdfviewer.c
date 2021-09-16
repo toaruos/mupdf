acb29fad1181c386fe9327a3f1bc4562ca6e16f9
K. Lange <klange@toaruos.org>
2021-09-16 15:51:35 +0900

    Present file picker on File > Open

--- toaru-app/pdfviewer.c
+++ toaru-app/pdfviewer.c
@@ -20,6 +20,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <sys/wait.h>
 #include <fitz.h>
 
 #include <toaru/yutani.h>
@@ -30,6 +31,7 @@
 #include "badmath.c"
 
 #define APPLICATION_TITLE "MuPDF"
+#define NAV_BAR_HEIGHT 0 //36
 
 gfx_context_t * gfx_ctx;
 yutani_t * yctx;
@@ -47,8 +49,7 @@ int getrusage(int who, void *usage) {
 	return -1;
 }
 
-enum { TEXT_PLAIN = 1, TEXT_HTML = 2, TEXT_XML = 3 };
-
+static char * docname = NULL;
 static float resolution = 72;
 static int res_specified = 0;
 static float rotation = 0;
@@ -56,44 +57,24 @@ static float rotation = 0;
 static int width = 0;
 static int height = 0;
 static int fit = 0;
-static int errored = 0;
-static int ignore_errors = 0;
-static int alphabits = 8;
 static int toggle_decors = 1;
 
-static fz_text_sheet *sheet = NULL;
-static fz_colorspace *colorspace;
-static char *filename;
-static int files = 0;
+static int end_page = 0;
+static int current_page  = 0;
+static int current_epage = 0;
+static fz_document * current_doc = NULL;
+static fz_context  * current_ctx = NULL;
 
 void draw_decors(int page, int epage) {
 	if (toggle_decors) {
 		char title[512] = {0};
-		sprintf(title, "%s - Page %d of %d", APPLICATION_TITLE, page, epage);
+		if (current_doc) {
+			sprintf(title, "%s - Page %d of %d", docname ? docname : APPLICATION_TITLE, page, epage);
+		} else {
+			sprintf(title, "%s", APPLICATION_TITLE);
+		}
 		render_decorations(window, gfx_ctx, title);
 	}
-}
-
-static void inplace_reorder(char * samples, int size) {
-	for (int i = 0; i < size; ++i) {
-		uint32_t c = ((uint32_t *)samples)[i];
-		char b = _RED(c);
-		char g = _GRE(c);
-		char r = _BLU(c);
-		char a = _ALP(c);
-		((uint32_t *)samples)[i] = rgba(r,g,b,a);
-	}
-}
-
-static int isrange(char *s)
-{
-	while (*s)
-	{
-		if ((*s < '0' || *s > '9') && *s != '-' && *s != ',')
-			return 0;
-		s++;
-	}
-	return 1;
 }
 
 static void draw_menu(void) {
@@ -106,8 +87,7 @@ static void draw_menu(void) {
 	menu_bar_render(&menu_bar, gfx_ctx);
 }
 
-static void drawpage(fz_context *ctx, fz_document *doc, int pagenum)
-{
+static void drawpage(fz_context *ctx, fz_document *doc, int pagenum) {
 	fz_page *page;
 	fz_display_list *list = NULL;
 	fz_device *dev = NULL;
@@ -123,7 +103,7 @@ static void drawpage(fz_context *ctx, fz_document *doc, int pagenum)
 	}
 	fz_catch(ctx)
 	{
-		fz_throw(ctx, "cannot load page %d in file '%s'", pagenum, filename);
+		fz_throw(ctx, "cannot load page %d in file", pagenum);
 	}
 
 	float zoom;
@@ -192,7 +172,7 @@ static void drawpage(fz_context *ctx, fz_document *doc, int pagenum)
 
 	fz_try(ctx)
 	{
-		pix = fz_new_pixmap_with_bbox(ctx, colorspace, bbox);
+		pix = fz_new_pixmap_with_bbox(ctx, fz_device_bgr, bbox);
 
 		fz_clear_pixmap_with_value(ctx, pix, 255);
 
@@ -204,15 +184,13 @@ static void drawpage(fz_context *ctx, fz_document *doc, int pagenum)
 		fz_free_device(dev);
 		dev = NULL;
 
-		int size = fz_pixmap_height(ctx, pix) * fz_pixmap_width(ctx, pix);
-		inplace_reorder(fz_pixmap_samples(ctx, pix), size);
 		size_t x_offset = (width - fz_pixmap_width(ctx, pix)) / 2;
 		size_t y_offset = (height - fz_pixmap_height(ctx, pix)) / 2;;
 		if (toggle_decors) {
 			struct decor_bounds bounds;
 			decor_get_bounds(window, &bounds);
 			x_offset += bounds.left_width;
-			y_offset += bounds.top_height + MENU_BAR_HEIGHT;
+			y_offset += bounds.top_height + MENU_BAR_HEIGHT + NAV_BAR_HEIGHT;
 		}
 		for (int i = 0; i < fz_pixmap_height(ctx, pix); ++i) {
 			memcpy(&GFX(gfx_ctx, x_offset, y_offset + i), &fz_pixmap_samples(ctx, pix)[fz_pixmap_width(ctx, pix) * i * 4], fz_pixmap_width(ctx, pix) * 4);
@@ -238,20 +216,52 @@ static void drawpage(fz_context *ctx, fz_document *doc, int pagenum)
 	fz_free_page(doc, page);
 
 	fz_flush_warnings(ctx);
-
-	if (cookie.errors)
-		errored = 1;
 }
 
-int current_page  = 0;
-int current_epage = 0;
-fz_document * current_doc = NULL;
-fz_context  * current_ctx = NULL;
+static void fitz_load_file(char * filename) {
+	if (current_doc) {
+		fz_close_document(current_doc);
+		current_doc = NULL;
+	}
+	if (docname) {
+		free(docname);
+		docname = NULL;
+	}
+	fz_try(current_ctx) {
+		current_doc = fz_open_document(current_ctx, filename);
+	} fz_catch(current_ctx) {
+		fprintf(stderr, "Failed to load document.\n");
+		fz_close_document(current_doc);
+		current_doc = NULL;
+	}
+
+	if (current_doc) {
+
+		char * s = strrchr(filename,'/');
+		if (s) {
+			docname = strdup(s+1);
+		} else {
+			docname = strdup(filename);
+		}
+
+		current_page = 1;
+		end_page = fz_count_pages(current_doc);
+	}
+}
+
+static void redraw_window(void) {
+	if (current_doc) {
+		drawpage(current_ctx, current_doc, current_page);
+	} else {
+		draw_fill(gfx_ctx, rgb(127,127,127));
+	}
+	draw_decors(current_page, end_page);
+	draw_menu();
+	yutani_flip(yctx, window);
+}
 
 static void redraw_window_callback(struct menu_bar * self) {
-	draw_decors(current_page, current_epage);
-	draw_menu();
-	drawpage(current_ctx, current_doc, current_page);
+	redraw_window();
 }
 
 void recalc_size(int w, int h) {
@@ -262,7 +272,7 @@ void recalc_size(int w, int h) {
 		struct decor_bounds bounds;
 		decor_get_bounds(window, &bounds);
 		width  = w - bounds.left_width - bounds.right_width;
-		height = h - bounds.top_height - bounds.bottom_height - MENU_BAR_HEIGHT;
+		height = h - bounds.top_height - bounds.bottom_height - MENU_BAR_HEIGHT - NAV_BAR_HEIGHT;
 	}
 }
 
@@ -274,7 +284,6 @@ void toggle_decorations(void) {
 
 static void resize_finish(int w, int h) {
 	recalc_size(w, h);
-
 	yutani_window_resize_accept(yctx, window, w, h);
 
 	reinit_graphics_yutani(gfx_ctx, window);
@@ -291,142 +300,51 @@ static void resize_finish(int w, int h) {
 	yutani_flip(yctx, window);
 }
 
-static void drawrange(fz_context *ctx, fz_document *doc, char *range) {
-	int page, spage, epage, pagecount;
-	char *spec, *dash;
-
-	pagecount = fz_count_pages(doc);
-	spec = fz_strsep(&range, ",");
-	while (spec)
-	{
-		dash = strchr(spec, '-');
-
-		if (dash == spec)
-			spage = epage = pagecount;
-		else
-			spage = epage = atoi(spec);
-
-		if (dash)
-		{
-			if (strlen(dash) > 1)
-				epage = atoi(dash + 1);
-			else
-				epage = pagecount;
-		}
-
-		spage = fz_clampi(spage, 1, pagecount);
-		epage = fz_clampi(epage, 1, pagecount);
-
-		current_doc = doc;
-		current_ctx = ctx;
-		current_epage = epage;
-
-		for (page = spage; page <= epage; ) {
-			if (page == 0) page = 1;
-
-			current_page = page;
-
-			drawpage(ctx, doc, page);
-			draw_menu();
-			draw_decors(page, epage);
-
-			yutani_flip(yctx, window);
-
-			yutani_msg_t * m = NULL;
-			while (1) {
-				m = yutani_poll(yctx);
-				if (m) {
-					if (menu_process_event(yctx, m)) {
-						redraw_window_callback(NULL);
-					}
-					switch (m->type) {
-						case YUTANI_MSG_KEY_EVENT:
-							{
-								struct yutani_msg_key_event * ke = (void*)m->data;
-								if (ke->event.action == KEY_ACTION_DOWN) {
-									switch (ke->event.keycode) {
-										case KEY_ESCAPE:
-										case 'q':
-											yutani_close(yctx, window);
-											exit(0);
-											break;
-										case KEY_ARROW_LEFT:
-										case 'a':
-											page--;
-											goto _continue;
-										case KEY_ARROW_RIGHT:
-										case 's':
-											page++;
-											if (page > epage) page = epage;
-											goto _continue;
-										case KEY_F12:
-											toggle_decorations();
-											goto _continue;
-										default:
-											break;
-									}
-								}
-							}
-							break;
-						case YUTANI_MSG_SESSION_END:
-							yutani_close(yctx, window);
-							exit(0);
-							break;
-						case YUTANI_MSG_WINDOW_FOCUS_CHANGE:
-							{
-								struct yutani_msg_window_focus_change * wf = (void*)m->data;
-								yutani_window_t * win = hashmap_get(yctx->windows, (void*)(uintptr_t)wf->wid);
-								if (win) {
-									win->focused = wf->focused;
-									goto _continue;
-								}
-							}
-							break;
-						case YUTANI_MSG_RESIZE_OFFER:
-							{
-								struct yutani_msg_window_resize * wr = (void*)m->data;
-								resize_finish(wr->width, wr->height);
-								goto _continue;
-							}
-							break;
-						case YUTANI_MSG_WINDOW_MOUSE_EVENT:
-							{
-								struct yutani_msg_window_mouse_event * me = (void*)m->data;
-								if (me->wid == window->wid) {
-									int result = decor_handle_event(yctx, m);
-									switch (result) {
-										case DECOR_CLOSE:
-											return;
-										case DECOR_RIGHT:
-											/* right click in decoration, show appropriate menu */
-											decor_show_default_menu(window, window->x + me->new_x, window->y + me->new_y);
-											break;
-										default:
-											/* Other actions */
-											break;
-									}
-									menu_bar_mouse_event(yctx, window, &menu_bar, me, me->new_x, me->new_y);
-								}
-							}
-							break;
-						default:
-							break;
-					}
-				}
-				free(m);
-			}
-_continue:
-			free(m);
-		}
-
-		spec = fz_strsep(&range, ",");
-	}
-}
-
 static void _menu_action_help(struct MenuEntry * entry) {
 	/* show help documentation */
 	system("help-browser agpl3.trt &");
-	redraw_window_callback(NULL);
+	redraw_window();
+}
+
+static void _menu_action_open(struct MenuEntry * entry) {
+	int pipe_fds[2];
+	pipe(pipe_fds);
+	pid_t child_pid = fork();
+	if (!child_pid) {
+		dup2(pipe_fds[1], STDOUT_FILENO);
+		close(pipe_fds[0]);
+		execvp("file-browser",(char*[]){"file-browser","--picker",NULL});
+		exit(123);
+		return;
+	}
+
+	close(pipe_fds[1]);
+	char buf[1024];
+	size_t accum = 0;
+
+	do {
+		int r = read(pipe_fds[0], buf+accum, 1023-accum);
+		if (r == 0) break;
+		if (r < 0) {
+			return;
+		}
+		accum += r;
+	} while (accum < 1023);
+
+	waitpid(child_pid, NULL, WNOHANG);
+	buf[accum] = '\0';
+	if (accum && buf[accum-1] == '\n') {
+		buf[accum-1] = '\0';
+	}
+
+	if (!buf[0]) {
+		redraw_window();
+		return;
+	}
+
+	fprintf(stderr, "opening doc '%s'\n", buf);
+	fitz_load_file(buf);
+	redraw_window();
 }
 
 static void _menu_action_exit(struct MenuEntry * entry) {
@@ -441,17 +359,10 @@ static void _menu_action_about(struct MenuEntry * entry) {
 	sprintf(coords, "%d %d &", (int)window->x + (int)window->width / 2, (int)window->y + (int)window->height / 2);
 	strcat(about_cmd, coords);
 	system(about_cmd);
-	redraw_window_callback(NULL);
+	redraw_window();
 }
 
-
 int main(int argc, char **argv) {
-	fz_document *doc = NULL;
-	int c;
-	fz_context *ctx;
-
-	fz_var(doc);
-
 	yctx = yutani_init();
 
 	char * _width  = getenv("WIDTH");
@@ -467,6 +378,8 @@ int main(int argc, char **argv) {
 	menu_bar.set = menu_set_create();
 
 	struct MenuList * m = menu_create(); /* File */
+	menu_insert(m, menu_create_normal("open",NULL,"Open", _menu_action_open));
+	menu_insert(m, menu_create_separator());
 	menu_insert(m, menu_create_normal("exit",NULL,"Exit", _menu_action_exit));
 	menu_set_insert(menu_bar.set, "file", m);
 
@@ -478,75 +391,118 @@ int main(int argc, char **argv) {
 
 	struct decor_bounds bounds;
 	decor_get_bounds(NULL, &bounds);
-	window = yutani_window_create(yctx, width + bounds.width, height + bounds.height + MENU_BAR_HEIGHT);
+	window = yutani_window_create(yctx, width + bounds.width, height + bounds.height + MENU_BAR_HEIGHT + NAV_BAR_HEIGHT);
 	yutani_window_move(yctx, window, 50, 50);
-
 	yutani_window_advertise_icon(yctx, window, APPLICATION_TITLE, "mupdf");
 
 	gfx_ctx = init_graphics_yutani(window);
 	draw_fill(gfx_ctx,rgb(0,0,0));
 	render_decorations(window, gfx_ctx, APPLICATION_TITLE " - Loading...");
 
-	while ((c = fz_getopt(argc, argv, "wf")) != -1) {
-		switch (c) {
-			case 'f':
-				fit = 1;
-				break;
-		}
+	/* Configure Fitz */
+	current_ctx = fz_new_context(NULL, NULL, FZ_STORE_DEFAULT);
+	fz_set_aa_level(current_ctx, 8);
+
+	/* Load a file */
+	if (argc > 1) {
+		fitz_load_file(argv[1]);
 	}
 
-	ctx = fz_new_context(NULL, NULL, FZ_STORE_DEFAULT);
-	if (!ctx) {
-		fprintf(stderr, "Could not initialize fitz context.\n");
-		exit(1);
-	}
+	/* Draw once */
+	redraw_window();
 
-	fz_set_aa_level(ctx, alphabits);
-	colorspace = fz_device_rgb;
-
-	fz_try(ctx) {
-		while (fz_optind < argc)
-		{
-			fz_try(ctx)
-			{
-				filename = argv[fz_optind++];
-				files++;
-
-				fz_try(ctx)
-				{
-					doc = fz_open_document(ctx, filename);
-				}
-				fz_catch(ctx)
-				{
-					fz_throw(ctx, "cannot open document: %s", filename);
-				}
-
-				if (fz_optind == argc || !isrange(argv[fz_optind]))
-					drawrange(ctx, doc, "1-");
-				if (fz_optind < argc && isrange(argv[fz_optind]))
-					drawrange(ctx, doc, argv[fz_optind++]);
-
-				fz_close_document(doc);
-				doc = NULL;
+	while (1) {
+		yutani_msg_t * m = yutani_poll(yctx);
+		if (m) {
+			if (menu_process_event(yctx, m)) {
+				redraw_window();
 			}
-			fz_catch(ctx)
-			{
-				if (!ignore_errors)
-					fz_rethrow(ctx);
-
-				fz_close_document(doc);
-				doc = NULL;
-				fz_warn(ctx, "ignoring error in '%s'", filename);
+			switch (m->type) {
+				case YUTANI_MSG_KEY_EVENT:
+					{
+						struct yutani_msg_key_event * ke = (void*)m->data;
+						if (ke->event.action == KEY_ACTION_DOWN) {
+							switch (ke->event.keycode) {
+								case KEY_ESCAPE:
+								case 'q':
+									yutani_close(yctx, window);
+									exit(0);
+									break;
+								case KEY_ARROW_LEFT:
+								case 'a':
+									current_page--;
+									if (current_page == 0) current_page = 1;
+									redraw_window();
+									break;
+								case KEY_ARROW_RIGHT:
+								case 's':
+									current_page++;
+									if (current_page > end_page) current_page = end_page;
+									redraw_window();
+									break;
+								case KEY_F12:
+									toggle_decorations();
+									break;
+								default:
+									break;
+							}
+						}
+					}
+					break;
+				case YUTANI_MSG_SESSION_END:
+					yutani_close(yctx, window);
+					exit(0);
+					break;
+				case YUTANI_MSG_WINDOW_FOCUS_CHANGE:
+					{
+						struct yutani_msg_window_focus_change * wf = (void*)m->data;
+						yutani_window_t * win = hashmap_get(yctx->windows, (void*)(uintptr_t)wf->wid);
+						if (win) {
+							win->focused = wf->focused;
+							redraw_window();
+							break;
+						}
+					}
+					break;
+				case YUTANI_MSG_RESIZE_OFFER:
+					{
+						struct yutani_msg_window_resize * wr = (void*)m->data;
+						resize_finish(wr->width, wr->height);
+						break;
+					}
+					break;
+				case YUTANI_MSG_WINDOW_MOUSE_EVENT:
+					{
+						struct yutani_msg_window_mouse_event * me = (void*)m->data;
+						if (me->wid == window->wid) {
+							int result = decor_handle_event(yctx, m);
+							switch (result) {
+								case DECOR_CLOSE:
+									exit(0);
+									break;
+								case DECOR_RIGHT:
+									/* right click in decoration, show appropriate menu */
+									decor_show_default_menu(window, window->x + me->new_x, window->y + me->new_y);
+									break;
+								default:
+									/* Other actions */
+									break;
+							}
+							menu_bar_mouse_event(yctx, window, &menu_bar, me, me->new_x, me->new_y);
+						}
+					}
+					break;
+				default:
+					break;
 			}
 		}
-	}
-	fz_catch(ctx) {
-		fz_close_document(doc);
-		fprintf(stderr, "error: cannot draw '%s'\n", filename);
-		errored = 1;
+		free(m);
 	}
 
-	fz_free_context(ctx);
+	if (current_doc) {
+		fz_close_document(current_doc);
+	}
+	fz_free_context(current_ctx);
 
-	return (errored != 0);
+	return 0;
 }
